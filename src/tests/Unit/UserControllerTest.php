4dd424940e8863380fa684b06e499ad90228aa87
alexei <rpsher@bk.ru>
2024-07-08 16:06:16 +0300

    Unit test via fixtures

--- src/tests/Unit/UserControllerTest.php
+++ src/tests/Unit/UserControllerTest.php
@@ -1,64 +1,93 @@
 <?php
 
+declare(strict_types=1);
 
-namespace Unit;
+namespace App\Test\Unit;
 
 use App\Controller\UserController;
-use App\Model\UserModel;
-use PHPUnit\Framework\MockObject\Exception;
+use App\Entity\User;
+use App\Test\Resource\Fixture\UserFixtures;
+use Doctrine\DBAL\Exception;
 use PHPUnit\Framework\TestCase;
+use App\App;
+
+use function PHPUnit\Framework\assertEquals;
+
+require __DIR__ . '/../../bootstrap.php';
 
 class UserControllerTest extends TestCase
 {
-    /**
-     * @throws Exception
-     */
-    public function testGetUser()
+    private UserController $controller;
+
+    public function setUp(): void
     {
-        $userModelMock = $this->createMock(UserModel::class);
-        $userModelMock->expects($this->once())
-            ->method('find')
-            ->willReturn([
-                'id' => 1,
-                'username' => 'testuser'
-            ]);
-
-        $controller = new UserController($userModelMock);
-
-        $result = $controller->getUser(1);
-        $this->assertEquals(['id' => 1, 'username' => 'testuser'], $result);
+        App::getEntityManager()->beginTransaction();
+        $this->controller = new UserController();
+        App::getEntityManager()->getConnection()->executeStatement("ALTER SEQUENCE users_id_seq RESTART WITH 1");
+        App::getEntityManager()->getConnection()->executeStatement("UPDATE users SET id=nextval('users_id_seq')");
+        App::getEntityManager()->clear();
     }
 
-    /**
-     * @throws Exception
-     */
-    public function testRemove()
+    public function tearDown(): void
     {
-        $userModelMock = $this->createMock(UserModel::class);
-        $userModelMock->expects($this->once())
-            ->method('removeById')
-            ->with($this->equalTo(1));
-
-        $controller = new UserController($userModelMock);
-
-        $result = $controller->remove('1');
-        $this->assertTrue($result);
+        App::getEntityManager()->rollback();
     }
 
-    /**
-     * @throws Exception
-     * @test
-     */
-    public function testAdd()
+    public function testAddUser(): void
     {
-        $userModelMock = $this->createMock(UserModel::class);
-        $userModelMock->expects($this->once())
-            ->method('add')
-            ->with($this->equalTo('testuser'));
+        $login = 'test';
+        $mail = 'test@mail.ru';
 
-        $controller = new UserController($userModelMock);
+        $this->controller->add($login, $mail);
+        $user = $this->controller->getByLogin('test');
+        $isAdded = $user !== null;
 
-        $result = $controller->add('testuser');
-        $this->assertTrue($result);
+        self::assertTrue($isAdded);
+        self::assertEquals($user->getMail(), $mail);
+        self::assertEquals($user->getLogin(), $login);
+    }
+
+    public function testAddUserWithEmptyFields(): void
+    {
+        $login = 'test';
+        $mail = 'test@mail.ru';
+
+        self::assertFalse($this->controller->add('', $mail));
+        self::assertFalse($this->controller->add($login, ''));
+    }
+
+    /** @throws Exception */
+    public function testGetUser(): void
+    {
+        $userFixture = App::getFixturesLoader()->getFixture(UserFixtures::class);
+        $userFixture->load(App::getEntityManager());
+
+        $id = App::getEntityManager()->getRepository(User::class)->count();
+
+        $givenUser = $this->controller->getUser($id);
+        self::assertNotNull($givenUser);
+        self::assertEquals(
+            $givenUser,
+            $this->controller->getByLogin($givenUser->getLogin())
+        );
+    }
+
+    public function testRemove(): void
+    {
+        $userFixture = App::getFixturesLoader()->getFixture(UserFixtures::class);
+        $userFixture->load(App::getEntityManager());
+
+        self::assertTrue($this->controller->remove('1'));
+        $givenUser = $this->controller->getUser(1);
+        self::assertNull($givenUser);
+        self::assertFalse($this->controller->remove('1'));
+    }
+
+    public function testGetAll()
+    {
+        $controllerCount = count($this->controller->getAll());
+        $doctrineCount = App::getEntityManager()->getRepository(User::class)->count();
+
+        assertEquals($controllerCount, $doctrineCount);
     }
 }
