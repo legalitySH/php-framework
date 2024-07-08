4b1399dade832218b44cab573b08e34454b3deec
alexei <rpsher@bk.ru>
2024-07-08 16:04:59 +0300

    added new routes

--- src/routes.php
+++ src/routes.php
@@ -5,38 +5,94 @@ declare(strict_types=1);
 require './vendor/autoload.php';
 
 use Bramus\Router\Router;
+use App\App;
+use App\Controller\CookieController;
+use App\Controller\SessionController;
 
 $router = new Router();
 
-$router->get('/users/(\d+)', function (int $id) {
-    echo "User ID: $id";
-});
 
 $router->get('/', function () {
-    $userController = new \App\Controller\UserController();
-    require './App/View/homepage.php';
+    echo App::getTwigProvider()->render('homepage.twig', ['title' => "Homepage"]);
 });
 
-$router->set404(function () {
-    header('HTTP/1.1 404 Not Found');
-    echo '404, route not found';
+$router->get('/cookies-example', function () {
+    $controller = new CookieController();
+    echo App::getTwigProvider()->render('cookies.twig', [
+        'title' => 'Cookies example',
+        'cookies' => $controller->getAllCookies()
+    ]);
 });
 
-$router->get('/update', function () {
-    $userController = new \App\Controller\UserController();
-    echo json_encode($userController->getAll());
+$router->post('/add-cookie', function () {
+    $controller = new CookieController();
+    $controller->createCookie(
+        $_POST['cookieKey'],
+        $_POST['cookieValue'],
+        $_POST['cookieExpirationTime']
+    );
+    header('Content-Type: application/json');
+    echo json_encode($_POST);
 });
 
-$router->post('/remove', function () {
-    $userController = $userController = new \App\Controller\UserController();
-    $userController->remove($_POST['id']);
+$router->get('/fetch-cookies', function () {
+    $controller = new CookieController();
+    header('Content-Type: application/json');
+    echo json_encode($controller->getAllCookies());
 });
 
-$router->post('/add', function () {
-    $userController = $userController = new \App\Controller\UserController();
-    $userController->add($_POST['login']);
+$router->post('/remove-cookie', function () {
+    $controller = new CookieController();
+    $postedData = $_POST['delete-cookie'];
+    $returnValue = $_SESSION[$postedData];
+    $controller->remove($postedData);
+    header('Content-Type: application/json');
+    echo json_encode($postedData);
+});
+
+$router->get('/sessions-example', function () {
+    ob_start();
+    require __DIR__ . '/App/View/session.php';
+    $contentBlock = ob_get_clean();
+
+    echo App::getTwigProvider()->render('sessions.twig', [
+        'title' => 'Session example',
+        'contentBlock' => $contentBlock
+    ]);
+});
+
+$router->post('/start-session', function () {
+    SessionController::start();
+    echo json_encode([
+        'session_status' => $_SERVER
+    ]);
+});
+
+$router->get('/session-status', function () {
+    echo json_encode(['session_status' => $_SERVER]);
+});
+
+$router->post('/abort-session', function () {
+    SessionController::abort();
+    echo json_encode([
+        'session_status' => $_SERVER
+    ]);
+});
+
+$router->post('/set-input-data', function () {
+    $controller = new SessionController();
+
+    if (session_status() === PHP_SESSION_ACTIVE) {
+        $_SESSION['inputData'] = $_POST['input-data'];
+        echo json_encode(['status' => $_SESSION['inputData']]);
+        return;
+    }
+
+    echo json_encode(['status' => 'Session not started']);
+});
+
+$router->get('/get-input-data', function () {
+    echo json_encode(['data' => $_SESSION['inputData']]);
 });
 
 $router->run();
-
-
