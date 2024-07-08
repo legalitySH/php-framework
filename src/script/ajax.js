ec2220360e7961329f1e4b43232d1f5fac578384
alexei <rpsher@bk.ru>
2024-07-08 15:57:36 +0300

    ajax script updating

--- src/script/ajax.js
+++ src/script/ajax.js
@@ -9,93 +9,15 @@ $(document).ready(function () {
         removeCookie($(this));
     });
 
-    $('#start-session-btn').on('click', function () {
-        startSession();
-    });
-
-    $('#abort-session-btn').on('click', function () {
-        abortSession();
-    });
-
-    $('#text-area').on('input', function () {
-        setSessionData();
-    });
-
     $(window).on('load', function () {
         fetchCookies();
-        fetchSessionStatus();
-        getSessionData();
     })
 
 });
 
 
 setInterval(fetchCookies, 1000);
-setInterval(fetchSessionStatus, 1000);
 
-
-function getSessionData() {
-    $.ajax({
-        url: '/get-input-data',
-        type: 'GET',
-        dataType: 'json',
-        success: function (data) {
-            console.log(data);
-            $('#text-area').val(data.data);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.error('Error fetching cookies:', textStatus, errorThrown);
-        }
-    });
-
-}
-
-function setSessionData() {
-    $.ajax({
-        url: '/set-input-data',
-        type: 'POST',
-        data: {'input-data': $('#text-area').val()},
-        dataType: 'json',
-        success: function (data) {
-            console.log(data);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.error('Error fetching cookies:', textStatus, errorThrown);
-        }
-    });
-
-}
-
-function abortSession() {
-    $.ajax({
-        url: '/abort-session',
-        type: 'POST',
-        dataType: 'json',
-        success: function (data) {
-            console.log(data);
-            fetchSessionStatus();
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.error('Error fetching cookies:', textStatus, errorThrown);
-        }
-    });
-
-}
-
-function startSession() {
-    $.ajax({
-        url: '/start-session',
-        type: 'POST',
-        dataType: 'json',
-        success: function (data) {
-            console.log(data);
-            fetchSessionStatus();
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.error('Error fetching cookies:', textStatus, errorThrown);
-        }
-    });
-}
 
 function removeCookie(form) {
 
@@ -162,21 +84,3 @@ function fetchCookies() {
     });
 }
 
-function fetchSessionStatus() {
-    $.ajax({
-        url: '/session-status',
-        type: 'GET',
-        dataType: 'json',
-        success: function (data) {
-            let isStarted = data.status;
-            if (isStarted) {
-                $('#session-status').text('Active');
-            } else {
-                $('#session-status').text('Not active');
-            }
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.error('Error fetching cookies:', textStatus, errorThrown);
-        }
-    });
-}
