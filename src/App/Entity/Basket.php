6a1ffd9c0d1e030dbcf48b8126b75a918de5f969
alexei <rpsher@bk.ru>
2024-07-10 17:07:43 +0300

    Basket entity example

---
+++ src/App/Entity/Basket.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\BasketRepository;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToMany;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity(repositoryClass: BasketRepository::class)]
+#[Table(name: 'basket')]
+class Basket
+{
+    #[Id]
+    #[Column]
+    #[GeneratedValue(strategy: "AUTO")]
+    private int $id;
+
+    #[ManyToOne(targetEntity: Product::class)]
+    #[JoinColumn(name: 'product_id', referencedColumnName: 'id', nullable: false)]
+    private Product $product;
+
+    #[ManyToOne(targetEntity: User::class)]
+    #[JoinColumn(name: 'user_id', referencedColumnName: 'id', nullable: false)]
+    private User $user;
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
+
+    #[Column]
+    private int $count;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setProduct(Product $product): void
+    {
+        $this->product = $product;
+    }
+
+    public function getCount(): int
+    {
+        return $this->count;
+    }
+
+    public function setCount(int $count): void
+    {
+        $this->count = $count;
+    }
+}
